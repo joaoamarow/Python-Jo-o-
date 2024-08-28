48216b3355a0f8447b81176938df7ab4a7103061
joaoamarow <163684273+joaoamarow@users.noreply.github.com>
2024-08-28 16:17:12 -0300

    Add files via upload

---
+++ Programador junior/main.py
@@ -0,0 +1,23 @@
+'''
+Autor: João Henrique Amaro Juliani
+Data:28/08/2024
+Versão: 1.0
+'''
+cargo = "namorada"
+nome = "Júlia Lima Gonçalvez"
+Curso = "Enfermagem"
+Faculdade = "uninove"
+idade = "21 anos"
+idade = str(idade)
+
+print("Eu amo a minha " + cargo + " chamada " + nome + " e estuda " + Curso + " na " + Faculdade + " e tem " + idade)
+
+
+
+# Eu amo a minha namorada linda chamda Jùlia Lima Gonçalvez e estuda enfermagem na uninove e tem 21 anos.
+
+
+
+
+
+
